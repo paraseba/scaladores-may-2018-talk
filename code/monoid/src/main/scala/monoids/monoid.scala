7c1dd06e432fb06c16f7a30080df03464cb734ef
Sebastian Galkin <paraseba@gmail.com>
2018-05-16 23:17:11 -0300

    Added code and tests

---
+++ code/monoid/src/main/scala/monoids/monoid.scala
@@ -0,0 +1,239 @@
+package monoids
+
+import scala.collection.immutable._
+
+
+trait Monoid[M] {
+
+  def zero: M
+
+  def append(a: M, b: => M): M
+
+}
+
+object Monoid {
+  def apply[A:Monoid]: Monoid[A] = implicitly[Monoid[A]]
+}
+
+object MonoidSyntax {
+  @SuppressWarnings(Array("org.wartremover.warts.ImplicitConversion"))
+  implicit def ToMonoidOps[A:Monoid](v: A): MonoidOps[A] =
+    new MonoidOps[A](v)
+
+  final class MonoidOps[A:Monoid](val self: A) {
+    def |+|(other: => A): A =
+      Monoid[A].append(self, other)
+  }
+}
+
+object SimpleMonoids {
+  import MonoidSyntax._
+
+  implicit def intAddMon: Monoid[Int] = new Monoid[Int] {
+    def zero = 0
+    def append(a: Int, b: => Int): Int = a + b
+  }
+
+
+  // is this really a monoid?
+  implicit def floatAddMon: Monoid[Float] = new Monoid[Float] {
+    def zero = 0
+    def append(a: Float, b: => Float): Float = a + b
+  }
+
+  implicit def intMulMon: Monoid[Int] = new Monoid[Int] {
+    def zero = 1
+    def append(a: Int, b: => Int): Int = a * b
+  }
+
+  implicit def freeMon[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    def zero: List[A] = List()
+    def append(as: List[A], bs: => List[A]): List[A] = as ++ bs
+  }
+
+  implicit def firstMon[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def zero = None
+    def append(a: Option[A], b: => Option[A]): Option[A] = a orElse b
+  }
+
+  def optionMon[A:Monoid]: Monoid[Option[A]] =
+    new Monoid[Option[A]] {
+
+      def zero: Option[A] = None
+
+      def append(a: Option[A], b: => Option[A]): Option[A] =
+        (a,b) match {
+          case (a, None) => a
+          case (None, b) => b
+          case (Some(a), Some(b)) => Some(a |+| b)
+        }
+  }
+
+  implicit def endoMon[A]: Monoid[A => A] = new Monoid[A => A] {
+    def zero: A => A = identity
+    def append(f: A => A, g: => (A => A)): A => A = g andThen f
+  }
+
+  implicit def monFunMon[A, B:Monoid]: Monoid[A => B] =
+    new Monoid[A => B] {
+
+      def zero: A => B = _ => Monoid[B].zero
+
+      def append(f: A => B, g: => (A => B)): A => B =
+        a => f(a) |+| g(a)
+  }
+
+  //(implicit am: Monoid[A], implicit bm: Monoid[B])
+  implicit def pairMon[A: Monoid, B: Monoid]: Monoid[(A,B)] = new Monoid[(A, B)] {
+
+    def zero: (A,B) = (Monoid[A].zero, Monoid[B].zero)
+
+    def append(a: (A, B), b: => (A, B)): (A,B) =
+      (a._1 |+| b._1, a._2 |+| b._2)
+  }
+
+
+  def mconcat[A:Monoid](as: Traversable[A]): A =
+    as.foldLeft(Monoid[A].zero)(_ |+| _)
+
+  def sum(xs: Traversable[Int]): Int =
+    mconcat(xs)(intAddMon)
+
+  def first[A](xs: Traversable[Option[A]]): Option[A] =
+    mconcat(xs)(firstMon)
+
+  def foldMap[A, M:Monoid](f: A => M)(as: Traversable[A]): M =
+    as.foldRight(Monoid[M].zero) { (x,res) =>
+      f(x) |+| res
+    }
+
+  val allMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    def zero: Boolean = true
+    def append(a: Boolean, b: => Boolean): Boolean = a && b
+  }
+
+
+  implicit val mon: Monoid[Boolean] = allMonoid
+
+  @SuppressWarnings(Array("org.wartremover.warts.Equals"))
+  val numbers: Iterable[Int] = 1.to(100).filter(
+    ((n:Int) => n % 2 == 0) |+| ((n:Int) => n >= 10) |+| ((n:Int) => n < 20)
+  )
+
+
+  sealed abstract class Min[A]
+  final case class EmptyMin[A]() extends Min[A]
+  final case class MinValue[A](value: A) extends Min[A]
+
+  object Min {
+    def apply[A](a: A): Min[A] = MinValue(a)
+
+
+    implicit def minMonoid[A:Ordering]: Monoid[Min[A]] = new Monoid[Min[A]] {
+      def zero = EmptyMin()
+
+      def append(a: Min[A], b: => Min[A]): Min[A] = (a, b) match {
+        case (EmptyMin(), x) => x
+        case (x, EmptyMin()) => x
+        case (MinValue(x), MinValue(y)) => Min(List(x,y).min)
+      }
+    }
+  }
+
+  sealed abstract class Max[A]
+  final case class EmptyMax[A]() extends Max[A]
+  final case class MaxValue[A](value: A) extends Max[A]
+
+  object Max {
+    def apply[A](a: A): Max[A] = MaxValue(a)
+
+    implicit def maxMonoid[A:Ordering]: Monoid[Max[A]] = new Monoid[Max[A]] {
+      def zero = EmptyMax()
+
+      def append(m1: Max[A], m2: => Max[A]): Max[A] = (m1, m2) match {
+        case (EmptyMax(), x) => x
+        case (x, EmptyMax())=> x
+        case (MaxValue(x), MaxValue(y)) => Max(List(x,y).max)
+      }
+    }
+  }
+
+  def min[A: Ordering](as: Traversable[A]): Option[A] = {
+    def mapper(a: A) = Min(a)
+
+    foldMap(mapper)(as) match {  // map and reduce
+      case MinValue(x) => Some(x)
+      case _ => None
+    }
+  }
+
+  def minmax[A: Ordering](as: Traversable[A]): Option[(A,A)] = {
+    def mapper(a: A) = (Min(a), Max(a))
+
+    foldMap(mapper)(as) match {
+      case (MinValue(x), MaxValue(y)) => Some((x,y))
+      case _ => None
+    }
+  }
+
+}
+
+object Parallel {
+
+  import MonoidSyntax._
+
+  def mconcat[A:Monoid](as: Traversable[A]): A =
+    as.par.fold(Monoid[A].zero)(_ |+| _)
+}
+
+object Stats {
+
+  sealed abstract class MeanVar
+  final case object EmptyMeanVar extends MeanVar
+  final case class MeanVarV(m1: Double, m2: Double, n: Long) extends MeanVar
+
+  object MeanVar {
+    import SimpleMonoids.foldMap
+
+    def singleton(x: Double): MeanVar = MeanVarV(x, 0, 1)
+
+    def sample(xs: Traversable[Double]): MeanVar =
+      foldMap(singleton(_))(xs)
+
+    def mean: MeanVar => Option[Double] = {
+      case MeanVarV(m1, _, _) => Some(m1)
+      case _ => None
+    }
+
+    def variance: MeanVar => Option[Double] = {
+      case MeanVarV(_, _, 1) => Some(0)
+      case MeanVarV(_, m2, n) => Some(m2/(n - 1.0))
+      case _ => None
+    }
+
+    def sampleSize: MeanVar => Long = {
+      case MeanVarV(_, _, n) => n
+      case _ => 0
+    }
+
+    implicit val mvMonoid: Monoid[MeanVar] = new Monoid[MeanVar] {
+      def zero: MeanVar = EmptyMeanVar
+
+      def append(a: MeanVar, b: => MeanVar): MeanVar = (a, b) match {
+        case (MeanVarV(m1a, m2a, na), MeanVarV(m1b, m2b, nb)) => {
+          val nt = na + nb
+          val delta = m1b - m1a
+          MeanVarV((na * m1a + nb * m1b ) / nt,
+                   m2a + m2b + delta * delta * na * nb / nt,
+                   nt)
+        }
+
+        case (EmptyMeanVar, a) => a
+        case (a, EmptyMeanVar) => a
+      }
+    }
+  }
+
+
+
+}
---
+++ code/monoid/src/test/scala/monoids/monoidTest.scala
@@ -0,0 +1,281 @@
+package monoids
+
+import scala.annotation.tailrec
+import org.scalatest._
+import org.scalatest.prop._
+import org.scalacheck._
+import org.scalacheck.Prop.{forAll}
+import org.scalactic.anyvals.PosInt
+
+import scala.collection.immutable._
+
+import Arbitrary.arbitrary
+import SimpleMonoids._
+
+
+class HelloSpec extends FunSuite with Checkers with Matchers {
+  import MonoidSyntax._
+
+  def assoc[M:Monoid](f: (M,M) => Boolean)(a: M, b: M, c: M): Boolean =
+    f((a |+| b) |+| c, a |+| (b |+| c))
+
+  def leftId[M:Monoid](f: (M,M) => Boolean)(a: M): Boolean =
+    f(Monoid[M].zero |+| a, a)
+
+  def rightId[M:Monoid](f: (M,M) => Boolean)(a: M): Boolean =
+    f(a |+| Monoid[M].zero, a)
+
+  def monoidLawsE[M:Monoid](f: (M,M) => Boolean)(a: M, b:M, c:M): Boolean =
+    assoc(f)(a,b,c) &&
+      leftId(f)(a) && leftId(f)(b) && leftId(f)(c) &&
+      rightId(f)(a) && rightId(f)(b) && rightId(f)(c)
+
+  def monoidLaws[M:Monoid](a: M, b:M, c:M): Boolean = monoidLawsE((a:M,b:M) => a === b)(a,b,c)
+
+  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
+    PropertyCheckConfiguration(minSuccessful = PosInt(1000), workers = 8)
+
+  test("intAddMon is a lawful monoid") {
+    check((a: Int, b: Int, c: Int) => monoidLaws(a,b,c)(intAddMon))
+  }
+
+  // This test is expected to fail, (Float, 0, +) is not a monoid
+  ignore("Float Sum is not a monoid!") {
+    check((a: Float, b: Float, c: Float) => monoidLaws(a,b,c)(floatAddMon))
+  }
+
+  test("intMulMon is a lawful monoid") {
+    check((a: Int, b: Int, c: Int) => monoidLaws(a,b,c)(intMulMon))
+  }
+
+  test("freeMon is a lawful monoid") {
+    check((a: List[Int], b: List[Int], c: List[Int]) => monoidLaws(a,b,c)(freeMon))
+  }
+
+  test("firstMon is a lawful monoid") {
+    check((a: Option[Int], b: Option[Int], c: Option[Int]) => monoidLaws(a,b,c)(firstMon))
+  }
+
+  test("optionMon is a lawful monoid") {
+    // use a non-commutative underlying monoid
+    check((a: Option[List[Int]], b: Option[List[Int]], c: Option[List[Int]]) =>
+      monoidLaws(a,b,c)(optionMon(freeMon)))
+  }
+
+  // fixme: missing tests for endoMon and monFunMon
+
+  test("pairMon is a lawful monoid") {
+    // use a non-commutative underlying monoids
+    check((a: (List[Int], Option[Int]), b: (List[Int], Option[Int]), c: (List[Int], Option[Int])) =>
+      monoidLaws(a,b,c)(pairMon))
+  }
+
+  test("mconcat of Lists is flatten") {
+    check((as: List[List[Int]]) =>
+      mconcat(as) === as.flatten
+    )
+  }
+
+  test("sum adds") {
+    check((as: List[Int]) =>
+      sum(as) === as.sum
+    )
+  }
+
+  test("first returns first Some") {
+    check((as: List[Option[Int]]) =>
+      first(as) === as.find(_.isDefined).flatten
+    )
+  }
+
+  test("foldMap maps and reduces") {
+    check((as: List[Int]) =>
+      foldMap((a: Int) => List(a,a))(as) === as.flatMap(a => List(a,a))
+    )
+  }
+
+  test("allMonoid is a lawful monoid") {
+    check((a: Boolean, b: Boolean, c: Boolean) =>
+      monoidLaws(a,b,c)(allMonoid))
+  }
+
+  test("allMonoid mconcat == and") {
+    check((as: List[Boolean]) =>
+      mconcat(as) === as.forall(identity)
+    )
+  }
+
+  test("appending predicates is and") {
+    numbers === 1.to(100)
+      .filter(_ % 2 === 0)
+      .filter(_ >= 10)
+      .filter(_ < 20)
+  }
+
+  def minGenerator[A:Arbitrary]: Gen[Min[A]] = arbitrary[Option[A]].map {
+    case None => EmptyMin()
+    case Some(n) => Min(n)
+  }
+
+  def maxGenerator[A:Arbitrary]: Gen[Max[A]] = arbitrary[Option[A]].map {
+    case None => EmptyMax()
+    case Some(n) => Max(n)
+  }
+
+  implicit def arbMin[A:Arbitrary]: Arbitrary[Min[A]] = Arbitrary(minGenerator)
+  implicit def arbMax[A:Arbitrary]: Arbitrary[Max[A]] = Arbitrary(maxGenerator)
+
+
+  test("minMonoid is a lawful monoid") {
+    check((a: Min[Int], b: Min[Int], c: Min[Int]) =>
+      monoidLaws(a,b,c)(Min.minMonoid))
+  }
+
+  test("minMonoid computes minimum") {
+    check((a: Min[Int], b: Min[Int]) => (a,b) match {
+            case (MinValue(aa), MinValue(bb)) => (a |+| b) === Min(List(aa,bb).min)
+            case (EmptyMin(), b) => (a |+| b) === b
+            case (a, EmptyMin()) => (a |+| b) === a
+          }
+    )
+  }
+
+  test("maxMonoid is a lawful monoid") {
+    check((a: Max[Int], b: Max[Int], c: Max[Int]) =>
+      monoidLaws(a,b,c)(Max.maxMonoid))
+  }
+
+  test("maxMonoid computes maximum") {
+    check((a: Max[Int], b: Max[Int]) => (a,b) match {
+            case (MaxValue(aa), MaxValue(bb)) => (a |+| b) === Max(List(aa,bb).max)
+            case (EmptyMax(), b) => (a |+| b) === b
+            case (a, EmptyMax()) => (a |+| b) === a
+          }
+    )
+  }
+
+  test("min computes minimum") {
+    check((as: Vector[Int]) => min(as) match {
+            case Some(m) => m === as.min
+            case None => as.isEmpty
+          })
+  }
+
+  test("minmax computes minimum and maximum") {
+    check((as: Vector[Int]) => minmax(as) match {
+            case Some((min,max)) => min === as.min && max === as.max
+            case None => as.isEmpty
+          })
+  }
+
+  test("Parallel same result") {
+    // use non-commutative underlying monoid
+    check((as: Vector[List[Int]]) => Parallel.mconcat(as) === mconcat(as))
+  }
+
+  test("Parallel faster") {
+    val slowMonoid = new Monoid[Int] {
+      def zero = 0
+
+      def append(a: Int, b: => Int): Int = {
+        val startTime = System.nanoTime();
+
+        @tailrec
+        def spin(ms: Int): Unit = {
+          val sleepTime = ms*1000000L;
+          if ((System.nanoTime() - startTime) < sleepTime) spin(ms)
+        }
+
+        spin(2)
+        a + b
+      }
+    }
+
+    def time[R](block: => R): Long = {
+      val t0 = System.nanoTime()
+      val _ = block
+      val t1 = System.nanoTime()
+      t1 - t0
+    }
+
+    val longVectors: Gen[Traversable[Int]] =
+      Gen.chooseNum(300, 600).flatMap {n =>
+        Gen.containerOfN[Traversable, Int](n, arbitrary[Int])
+      }
+
+    check(
+      forAll(longVectors)(
+        as => {
+          val timeSeq = time(mconcat(as)(slowMonoid))
+          val timePar = time(Parallel.mconcat(as)(slowMonoid))
+          timeSeq > timePar
+        })
+        , minSuccessful(50))
+    }
+
+  import Stats._
+
+  val sampleGen: Gen[Double] = Gen.choose(-1000.0,1000.0)
+  val samplesGen: Gen[Vector[Double]] = Gen.containerOf[Vector, Double](sampleGen)
+  val mvGenerator: Gen[MeanVar] = samplesGen.map(MeanVar.sample)
+
+  implicit val arbMeanV: Arbitrary[MeanVar] = Arbitrary(mvGenerator)
+
+  def eqMV(a: MeanVar, b: MeanVar): Boolean = (a,b) match {
+    case (MeanVarV(m1a,m2a,na), MeanVarV(m1b,m2b,nb)) =>
+      na === nb && m1a === m1b +- 0.0001 && m2a === m2b +- 0.0001
+    case (EmptyMeanVar, EmptyMeanVar) => true
+    case _ => false
+  }
+
+  test("MeanVar has a lawful monoid") {
+    check((a: MeanVar, b: MeanVar, c: MeanVar) =>
+      monoidLawsE(eqMV)(a,b,c))
+  }
+
+  test("MeanVar computes trivial statistics") {
+    check(
+      forAll(Gen.choose(1,100))(
+        size => {
+          val mv = MeanVar.sample(Vector.fill(size)(42.0))
+          MeanVar.mean(mv).get === (42.0 +- 1e-6) &&
+            MeanVar.variance(mv).get === (0.0 +- 1e-6) &&
+            MeanVar.sampleSize(mv) === size
+        }
+      )
+    )
+  }
+
+  val batchGen: Gen[Vector[Vector[Double]]] = for {
+    // total number of samples
+    n <- Gen.choose(1000, 2000)
+
+    // number of batches: samples will be splitted in this many parts
+    numBatches <- Gen.chooseNum[Int](1,100)
+
+    cuts <- Gen.containerOfN[Vector, Int](numBatches - 1, Gen.chooseNum(0, n-1)).map(_.sorted :+ n)
+    sizes = (cuts.+:(0), cuts).zipped.map((from,to) => to - from)
+    batches <- Gen.sequence[Vector[Vector[Double]], Vector[Double]](sizes.map (Gen.containerOfN[Vector, Double](_, sampleGen)))
+  } yield batches
+
+  test("MeanVar computes mean/var incrementally") {
+    import org.scalacheck.Shrink
+    implicit val noShrink: Shrink[Vector[Vector[Double]]] = Shrink.shrinkAny
+
+    check(
+      forAll(batchGen)(
+        groups => {
+          val mv = foldMap(MeanVar.sample)(groups)
+          val (expectedMean, expectedVar) = (0.0, 2000*2000/12.0)
+          val meanEstimatorDispersion = math.sqrt(expectedVar/MeanVar.sampleSize(mv))
+          val expectedKurtosis = 6.0/5
+          val varianceEstimatorDispersion =
+            math.sqrt(math.abs(expectedKurtosis - 1) * expectedVar * expectedVar / MeanVar.sampleSize(mv))
+          println(s"${MeanVar.variance(mv).get},${MeanVar.mean(mv).get}")
+          MeanVar.mean(mv).get === expectedMean +- 4*meanEstimatorDispersion &&
+            MeanVar.variance(mv).get === expectedVar +- 8*varianceEstimatorDispersion
+        }
+      )
+    )
+  }
+}
